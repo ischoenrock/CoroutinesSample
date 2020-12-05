578dd39415ecabda2153e4ef88424761651ce8c0
Ian Schoenrock <32747586+theapphideaway@users.noreply.github.com>
2020-12-04 22:19:59 -0700

    Added Coroutine example using a job

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,30 @@
-fun main(){
-    println("Hello World")
+import kotlinx.coroutines.*
+
+fun main() = runBlocking{
+    val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+    println("Start")
+    val job = coroutineScope.launch {
+        runCoroutine()
+        runSecondCoroutine()
+        runThirdCoroutine()
+    }
+
+    job.join()
+    println("Stop")
+}
+
+suspend fun runCoroutine(){
+    delay(2000)
+    println("First")
+}
+
+suspend fun runSecondCoroutine(){
+    delay(2000)
+    println("Second")
+}
+
+suspend fun runThirdCoroutine(){
+    delay(2000)
+    println("Third")
 }
